598dfb8de99218234ee2df61cfa520254b5c5660
WavRunja <pereverzew-wladimir@yandex.ru>
2024-05-26 08:13:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# Самостоятельная работа по уроку "Цели и задачи. Поток выполнения программы. Как интерпретатор показывает переменные."
+
+print('Hi, PyCharm')
+x = 43
+y = 32
+print(x * y)
+print("End line")
+
+#  https://github.com/WavRunja/UrbanUniversityPythonDeveloperMyHomework
