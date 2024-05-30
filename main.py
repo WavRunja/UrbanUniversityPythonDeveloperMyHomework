42c3838abd78807ac8c3efd86220f3014627bade
WavRunja <pereverzew-wladimir@yandex.ru>
2024-05-30 03:04:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-# Самостоятельная работа по уроку "Цели и задачи. Поток выполнения программы. Как интерпретатор показывает переменные."
+# Домашняя работа по уроку "Модули и пакеты"
 
-print('Hi, PyCharm')
-x = 43
-y = 32
-print(x * y)
-print("End line")
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+# Примеры вызовов функций
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+
+# Вывод результатов на экран
+print(result1)  # Ожидаемый вывод: 23.0
+print(result2)  # Ожидаемый вывод: 'Ошибка'
+print(result3)  # Ожидаемый вывод: 7.0
+print(result4)  # Ожидаемый вывод: inf
 
 #  https://github.com/WavRunja/UrbanUniversityPythonDeveloperMyHomework
